c0490e5216bef1c7a9cc294fd08a9f8455b06584
wayofthepie <stephen.obrien4@emc.com>
2018-03-15 21:40:12 +0000

    Update docs

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,20 +1,24 @@
 package config
 
 import (
-	"github.com/execd/task-store/pkg/model"
 	"github.com/BurntSushi/toml"
+	"github.com/execd/task-store/pkg/model"
 )
 
+// Parser : config parser
 type Parser interface {
 	ParseConfig(configPath string) *model.Config
 }
 
+// ParserImpl : implementation of a config parser
 type ParserImpl struct{}
 
+// NewParserImpl : build a new config parser
 func NewParserImpl() *ParserImpl {
 	return &ParserImpl{}
 }
 
+// ParseConfig : parse the given config file, panics if parsing fails
 func (p *ParserImpl) ParseConfig(configPath string) *model.Config {
 	config := new(model.Config)
 	_, err := toml.DecodeFile(configPath, config)
--- pkg/model/config.go
+++ pkg/model/config.go
@@ -1,9 +1,11 @@
 package model
 
+// Config : represents application configuration
 type Config struct {
 	Manager ManagerInfo
 }
 
+// ManagerInfo : config fo the manager section
 type ManagerInfo struct {
 	ExecutionQueueSize int `toml:"execution_queue_size"`
 	TaskQueueSize      int `toml:"task_queue_size"`
